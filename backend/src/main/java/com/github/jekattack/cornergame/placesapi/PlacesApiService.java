897d216086e59a6c2fb1f5b3d6a22bc7a230c4f4
Jan Kraus <jan.erik.kraus@t-online.de>
2022-07-12 15:42:18 +0200

    dragnet search for hamburgkiosks implemented

--- backend/src/main/java/com/github/jekattack/cornergame/placesapi/PlacesApiService.java
+++ backend/src/main/java/com/github/jekattack/cornergame/placesapi/PlacesApiService.java
@@ -17,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class PlacesApiService {
@@ -35,30 +36,47 @@ public class PlacesApiService {
 
     public List<KioskResponseData> getAllKiosksFromGoogle() {
 
-        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=53.556570,9.985951&radius=9000&keyword=kiosk&key=" + googleMapsApiKey;
+        int counter = 0;
         List<KioskResponseData> responseData = new ArrayList<>();
 
-        //Send first Request to GooglePlaces-Api
-        ResponseEntity<KioskResponseData> singleResponseData = restTemplate.getForEntity(url, KioskResponseData.class);
-        responseData.add(singleResponseData.getBody());
+        int latStart = 5376;
+        int lngStart = 1032;
+        int[] latSteps = IntStream.range(0, 34).toArray();
+        int[] lngSteps = IntStream.range(0, 58).toArray();
 
-        //GetRequest for next pages of initial Request
-        while(singleResponseData.getBody().getNextPageToken()!=null){
-            try{
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                LOGGER.error("Something interrupted the waiting period.");
+        for(int latStep : latSteps){
+            double lat = ((double) (latStart - latStep)) * 0.01;
+            for(int lngStep : lngSteps){
+                double lng = ((double) (lngStart - lngStep)) * 0.01;
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ lat +"," + lng +"&radius=2500&keyword=kiosk&key=" + googleMapsApiKey;
+
+                ResponseEntity<KioskResponseData> singleResponseData = restTemplate.getForEntity(url, KioskResponseData.class);
+                responseData.add(singleResponseData.getBody());
+
+                //GetRequest for next pages of initial Request
+                while(singleResponseData.getBody().getNextPageToken()!=null){
+                    try{
+                        Thread.sleep(2000);
+                        counter++;
+                        System.out.println(counter + singleResponseData.getBody().getResults()[0].getName());
+                    } catch (InterruptedException ex) {
+                        LOGGER.error("Something interrupted the waiting period.");
+                    }
+                    String urlNextPage = url + "&pagetoken=" + singleResponseData.getBody().getNextPageToken();
+                    singleResponseData = restTemplate.getForEntity(urlNextPage, KioskResponseData.class);
+                    responseData.add(singleResponseData.getBody());
+                }
+                LOGGER.info("Search process finished.");
             }
-            String urlNextPage = url + "&pagetoken=" + singleResponseData.getBody().getNextPageToken();
-            singleResponseData = restTemplate.getForEntity(urlNextPage, KioskResponseData.class);
-            responseData.add(singleResponseData.getBody());
         }
+
         LOGGER.info("Search process finished.");
 
         //Store Results
         kioskService.createManyKiosks(convertKioskResponse(responseData));
 
         return responseData;
+
     }
 
     public List<Kiosk> convertKioskResponse(List<KioskResponseData> kioskResponseData){
