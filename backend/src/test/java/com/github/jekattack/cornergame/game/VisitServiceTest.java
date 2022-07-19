a1fbe7701cbe7d9e13e9ca3e814abf46160be8fd
Jan Kraus <jan.erik.kraus@t-online.de>
2022-07-19 18:04:34 +0200

    minor changes in tests

---
+++ backend/src/test/java/com/github/jekattack/cornergame/game/VisitServiceTest.java
@@ -0,0 +1,86 @@
+package com.github.jekattack.cornergame.game;
+
+import com.github.jekattack.cornergame.kioskdata.Kiosk;
+import com.github.jekattack.cornergame.kioskdata.KioskRepository;
+import com.github.jekattack.cornergame.kioskdata.details.KioskLocation;
+import com.github.jekattack.cornergame.kioskdata.details.KioskLocationCoordinates;
+import com.github.jekattack.cornergame.userdata.CGUser;
+import com.github.jekattack.cornergame.userdata.CGUserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
+
+class VisitServiceTest {
+
+    @Test
+    void shouldCreateNewVisit(){
+        //Given
+        String testUsername = "bo";
+        String testGooglePlacesId = "ChIJ5Xr0R1CPsUcRwgzxHRvBnf4";
+        UserLocationCoordinates testUserLocationCoordinates = new UserLocationCoordinates(53.5736689, 9.9607373);
+        UserLocation testUserLocation = new UserLocation(testUserLocationCoordinates);
+        VisitCreationData testVisitCreationData = new VisitCreationData(testUserLocation, testGooglePlacesId);
+
+        VisitRepository testVisitRepository = Mockito.mock(VisitRepository.class);
+        Visit[] visitsOfTestUser = new Visit[0];
+        Mockito.when(testVisitRepository.findAllByUserId("testid")).thenReturn(visitsOfTestUser);
+
+        KioskRepository testKioskRepository = Mockito.mock(KioskRepository.class);
+        KioskLocationCoordinates testKioskLocationCoordinates = new KioskLocationCoordinates(53.5736689, 9.9607373);
+        KioskLocation testKioskLocation = new KioskLocation(testKioskLocationCoordinates);
+        Kiosk testKiosk = Kiosk.builder().kioskLocation(testKioskLocation).name("testKiosk").googlePlacesId("ChIJ5Xr0R1CPsUcRwgzxHRvBnf4").build();
+        Mockito.when(testKioskRepository.findByGooglePlacesId("ChIJ5Xr0R1CPsUcRwgzxHRvBnf4")).thenReturn(Optional.of(testKiosk));
+
+        CGUserRepository testCGUserRepository = Mockito.mock(CGUserRepository.class);
+        CGUser testUser = CGUser.builder().username("bo").id("testid").build();
+        Mockito.when(testCGUserRepository.findByUsername("bo")).thenReturn(Optional.of(testUser));
+
+        VisitService testVisitService = new VisitService(testVisitRepository, testKioskRepository, testCGUserRepository);
+
+        Visit expectedVisit = Visit.builder().userId("testid").googlePlacesId("ChIJ5Xr0R1CPsUcRwgzxHRvBnf4").timestamp(Date.from(Instant.now())).build();
+
+        //When
+        testVisitService.createVisit(testVisitCreationData, testUsername);
+
+        //Then
+        Mockito.verify(testVisitRepository).save(Mockito.argThat(v ->
+                Objects.equals(v.getGooglePlacesId(), expectedVisit.getGooglePlacesId()) &&
+                Objects.equals(v.getUserId(), expectedVisit.getUserId()) &&
+                ((v.getTimestamp().getTime() - expectedVisit.getTimestamp().getTime()) < 1000)));
+    }
+
+    @Test
+    void shouldNotCreateNewVisit(){
+        //Given
+        String testUsername = "bo";
+        String testGooglePlacesId = "ChIJ5Xr0R1CPsUcRwgzxHRvBnf4";
+        UserLocationCoordinates testUserLocationCoordinates = new UserLocationCoordinates(52.5736689, 9.9607373);
+        UserLocation testUserLocation = new UserLocation(testUserLocationCoordinates);
+        VisitCreationData testVisitCreationData = new VisitCreationData(testUserLocation, testGooglePlacesId);
+
+        VisitRepository testVisitRepository = Mockito.mock(VisitRepository.class);
+        Visit[] visitsOfTestUser = new Visit[0];
+        Mockito.when(testVisitRepository.findAllByUserId("testid")).thenReturn(visitsOfTestUser);
+
+        KioskRepository testKioskRepository = Mockito.mock(KioskRepository.class);
+        KioskLocationCoordinates testKioskLocationCoordinates = new KioskLocationCoordinates(53.5736689, 9.9607373);
+        KioskLocation testKioskLocation = new KioskLocation(testKioskLocationCoordinates);
+        Kiosk testKiosk = Kiosk.builder().kioskLocation(testKioskLocation).name("testKiosk").googlePlacesId("ChIJ5Xr0R1CPsUcRwgzxHRvBnf4").build();
+        Mockito.when(testKioskRepository.findByGooglePlacesId("ChIJ5Xr0R1CPsUcRwgzxHRvBnf4")).thenReturn(Optional.of(testKiosk));
+
+        CGUserRepository testCGUserRepository = Mockito.mock(CGUserRepository.class);
+        CGUser testUser = CGUser.builder().username("bo").id("testid").build();
+        Mockito.when(testCGUserRepository.findByUsername("bo")).thenReturn(Optional.of(testUser));
+
+        VisitService testVisitService = new VisitService(testVisitRepository, testKioskRepository, testCGUserRepository);
+
+        //Then
+        Assertions.assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> testVisitService.createVisit(testVisitCreationData, testUsername));
+    }
+
+}
